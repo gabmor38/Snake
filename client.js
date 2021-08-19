a594465917098b20f0b607a211020f84d83027b8
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-19 11:08:53 -0400

    created connect event handler and name of player

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ const net = require('net');
  */
 
 const connect = function() {
-  const conn = net.createConnection({
+  const conn = net.createConnection({  //conn object
     host: "192.168.0.10",
     port: 50541
   });
@@ -14,12 +14,16 @@ const connect = function() {
   // INTERPRET INCOMING DATA AS TEXT
   conn.setEncoding('utf8');
 
-// Add an even handler for incoming data.
-
+// Add an event handler for incoming data.
 conn.on('data', message => {
   console.log(message);
+});
+//Add another "connect" event handles for establishing a connection - send a message that connection is establish and write name of player
+conn.on('connect', () => {
+  console.log("Succesfully connected to game server");
+  conn.write("Name: GMR");
+});
 
-})
 
 return conn;
 
