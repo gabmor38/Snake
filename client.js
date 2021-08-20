34aafeeada39599c4a3e4803a1eb480eef34c53a
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-19 22:47:34 -0400

    linted the code

--- client.js
+++ client.js
@@ -1,4 +1,3 @@
-//play.js = move the connect function from play to here
 const net = require('net');
 const { IP, PORT } = require('./constants');
 
@@ -15,24 +14,19 @@ const connect = function() {
   // INTERPRET INCOMING DATA AS TEXT
   conn.setEncoding('utf8');
 
-// Add an event handler for incoming data.
+  // Add an event handler for incoming data.
   conn.on('data', message => {
     console.log(message);
   });
-//Add another "connect" event handles for establishing a connection - send a message that connection is establish and write name of player
+  //Add another "connect" event handles for establishing a connection - send a message that connection is establish and write name of player
   conn.on('connect', () => {
     console.log("Succesfully connected to game server");
     conn.write("Name: GMR");
-    // function write(){ // create a function with a setInterval to move the snake up - test
-    //   return conn.write("Move: up")
-    });
+  });
 
-  //   setInterval(write, 1000)
-  // });
+  return conn;
+};
 
-return conn;
-}
 console.log('Connecting ...');
 
-
-module.exports = { connect }
+module.exports = { connect };
